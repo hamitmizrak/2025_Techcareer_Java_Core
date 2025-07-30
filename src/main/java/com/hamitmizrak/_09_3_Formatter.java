ba9ffb6d26b83ba1492fc58a602bafdb099b9e80
hamitmizrak <hamitmizrak@gmail.com>
2025-07-30 12:13:46 +0300

    1- Comment 2- Psvm 3- Variable 4- Primitive Types 5- Wrapper Types 6- Number 7- Operator 8- Cast 9- Math 10- Random 11- Scanner 12- Escape 13- System 14- Gc 15- String 16- Method 17- Formatter

--- src/main/java/com/hamitmizrak/_09_3_Formatter.java
+++ src/main/java/com/hamitmizrak/_09_3_Formatter.java
@@ -1,7 +1,17 @@
 package com.hamitmizrak;
 
+import java.util.Formatter;
+
 public class _09_3_Formatter {
     public static void main(String[] args) {
 
+        // import java.util.Formatter;
+        String str="Hamit";
+        int decimal=13;
+        float floatingPoint=14.53f;
+        Formatter formatter = new Formatter();
+        formatter.format("Merhaba %s, deneyim %d tarih: %2.3f",str ,decimal,floatingPoint);
+        System.out.println(formatter);
+        formatter.close();
     }
 }
