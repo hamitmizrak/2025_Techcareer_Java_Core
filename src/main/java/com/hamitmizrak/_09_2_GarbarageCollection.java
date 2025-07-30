213b6144a4f7b4563d76895021efcb646da5cff4
hamitmizrak <hamitmizrak@gmail.com>
2025-07-30 11:48:01 +0300

    1- Comment 2- Psvm 3- Variable 4- Primitive Types 5- Wrapper Types 6- Number 7- Operator 8- Cast 9- Math 10- Random 11- Scanner 12- Escape 13- System 14- Gc 15- String

---
+++ src/main/java/com/hamitmizrak/_09_2_GarbarageCollection.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak;
+
+public class _09_2_GarbarageCollection {
+
+    public static void main(String[] args) {
+        System.gc();
+    }
+}
---
+++ src/main/java/com/hamitmizrak/_09_3_Formatter.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak;
+
+public class _09_3_Formatter {
+    public static void main(String[] args) {
+
+    }
+}
