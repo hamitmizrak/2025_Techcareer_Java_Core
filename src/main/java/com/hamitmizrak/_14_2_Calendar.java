f9155cc4d21740c47d9ad6ef6ec2673f6436be79
hamitmizrak <hamitmizrak@gmail.com>
2025-07-31 09:50:44 +0300

    1- Comment 2- Psvm 3- Variable 4- Primitive Types 5- Wrapper Types 6- Number 7- Operator 8- Cast 9- Math 10- Random 11- Scanner 12- Escape 13- System 14- Gc 15- String 16- Method 17- Formatter 18- Conditional 19- Loop 20- Date

---
+++ src/main/java/com/hamitmizrak/_14_2_Calendar.java
@@ -0,0 +1,40 @@
+package com.hamitmizrak;
+
+import java.util.Calendar;
+
+
+/**
+ Calendar: new Date'e alternatif olarak gelmiştir */
+public class _14_2_Calendar {
+
+    public static void calendarDate(){
+
+        // Calendar instance
+        Calendar cal = Calendar.getInstance();
+
+        System.out.println("Şu an: " + cal.getTime());
+
+        // Tarih parçalayıp yaz
+        int year = cal.get(Calendar.YEAR); // year
+        int month = cal.get(Calendar.MONTH) + 1; // ay
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        System.out.println("Yıl: "+ cal.get(Calendar.YEAR)); // 1900 eklemeye,çıkarmaya gerek yok
+
+        System.out.println("Tarih: " + day + "/" + month + "/" + year);
+
+        // 10 gün ekle
+        cal.add(Calendar.DAY_OF_MONTH, 10);
+        System.out.println("10 gün sonra: " + cal.getTime());
+
+        // Saat ayarla
+        cal.set(Calendar.HOUR_OF_DAY, 15);
+        cal.set(Calendar.MINUTE, 30);
+        System.out.println("Saat ayarı sonrası: " + cal.getTime());
+    }
+
+    // Formatter
+
+    public static void main(String[] args) {
+        calendarDate();
+    }
+}
