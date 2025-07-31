efef1ee02ea3296679b79b439f3da34ff9c68a36
hamitmizrak <hamitmizrak@gmail.com>
2025-07-31 10:35:44 +0300

    1- Comment 2- Psvm 3- Variable 4- Primitive Types 5- Wrapper Types 6- Number 7- Operator 8- Cast 9- Math 10- Random 11- Scanner 12- Escape 13- System 14- Gc 15- String 16- Method 17- Formatter 18- Conditional 19- Loop 20- Date 21- Exception 22- Enum 23- Class

---
+++ src/main/java/com/hamitmizrak/_18_1_POJO_1_Class.java
@@ -0,0 +1,29 @@
+package com.hamitmizrak;
+
+// POJO : Plain Old Java Object
+// POJO : Constructor olabilir veya olmayabilir
+// Access Modifier(Erişim belirleyiciler) : public, private, protected, - gelebilir
+
+public class _18_1_POJO_1_Class {
+
+    // Field
+    private String name;
+    public String surname;
+
+    // GETTER AND SETTER
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
